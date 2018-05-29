49a19ab7c7544b4d9abc0c28ead49e7602913f77
deepak-ranolia <d.ranolia92@gmail.com>
2018-05-30 02:02:27 +0530

    full stack step 1

---
+++ upload.js
@@ -0,0 +1,18 @@
+const IncomingForm = require( 'formidable' ).IncomingForm
+
+module.exports = function upload( req, res ) {
+    var form = new IncomingForm()
+    print( form )
+    form.on( 'file' , ( field, file ) => {
+        console.log(file)
+        console.log(field)
+    // Do something with the file
+    // e.g. save it to the database
+    // you can access it using file.path
+    })
+    form.on('end', () => {
+        res.json()
+    })
+    form.parse(req)
+}
+
